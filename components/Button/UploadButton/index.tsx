88de5a2b959dfd4e45852f65fe91449b116c179b
@gopal1725740 <gopal@code-b.in>
2024-02-16 23:54:16 +0530

    resolve conflict

--- components/Button/UploadButton/index.tsx
+++ components/Button/UploadButton/index.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 // not allowed
 // ==============================================
 // Filename: CBUploader.tsx
@@ -6,11 +6,11 @@
 // Last Updated: Nov 17 2023
 // Project: cb-website - Front End
 // ==============================================
-import React, { useEffect, useState } from 'react';
-import { ButtonSize, ButtonVariant, UploadButtonType } from '@/types/index';
-import { faCloudArrowUp } from '@fortawesome/free-solid-svg-icons';
-import Button from '..';
-import { Typography, TypographyVariant } from '../..';
+import React, { useEffect, useState } from "react";
+import { ButtonSize, ButtonVariant, UploadButtonType } from "@/types/index";
+import { faCloudArrowUp } from "@fortawesome/free-solid-svg-icons";
+import Button from "../index";
+import Typography, { TypographyVariant } from "@/components/Typography";
 
 const CBUploader = ({
   handleFileChange,
@@ -18,44 +18,44 @@ const CBUploader = ({
   errorText,
   isDarkBackground,
 }: UploadButtonType) => {
-  const [sideText, setSideText] = useState('Max file size 5MB');
+  const [sideText, setSideText] = useState("Max file size 5MB");
   useEffect(() => {
     if (errorText) {
       setSideText(errorText);
     } else if (file) {
       setSideText(file.name);
     } else {
-      setSideText('Max file size 5MB');
+      setSideText("Max file size 5MB");
     }
   }, [errorText, file, sideText]);
   return (
     <div>
       <input
-        style={{ display: 'none' }}
-        id='upload-resume'
+        style={{ display: "none" }}
+        id="upload-resume"
         onChange={(e) => handleFileChange(e)}
-        type='file'
+        type="file"
       />
-      <div className='flex items-center gap-2'>
+      <div className="flex items-center gap-2">
         <Button
           onClick={() => {
-            const fileInput = document.getElementById('upload-resume');
+            const fileInput = document.getElementById("upload-resume");
             if (fileInput) {
               fileInput.click();
             }
           }}
-          title='Upload Resume'
+          title="Upload Resume"
           variant={ButtonVariant.SECONDARY}
           size={ButtonSize.MEDIUM}
           icon={faCloudArrowUp}
-          backgroundColor={isDarkBackground ? 'white' : 'black'}
-          color={isDarkBackground ? 'black' : 'white'}
+          backgroundColor={isDarkBackground ? "white" : "black"}
+          color={isDarkBackground ? "black" : "white"}
         />
-        <div className='max-w-xs overflow-hidden'>
+        <div className="max-w-xs overflow-hidden">
           <Typography
             variant={TypographyVariant.LABELLIGHT}
             className={`${
-              errorText ? 'text-red' : 'text-white'
+              errorText ? "text-red" : "text-white"
             } overflow-hidden overflow-ellipsis whitespace-nowrap`}
           >
             {sideText}
